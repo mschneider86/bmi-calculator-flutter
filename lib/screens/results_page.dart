88ff3ab00f04eefe47dc0836e12a871242ea1d29
Matheus Schneider <matheus.ssm@gmail.com>
2020-09-17 11:50:49 -0300

    added: method to calculate the bmi

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -5,6 +5,13 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class ResultsPage extends StatelessWidget {
+
+  ResultsPage({@required this.interpretation, @required this.bmiResult, @required this.resultText});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,15 +41,15 @@ class ResultsPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      'Normal',
+                      resultText.toUpperCase(),
                       style: kResultTextStyle,
                     ),
                     Text(
-                      '18.3',
+                      bmiResult,
                       style: kBMITextStyle,
                     ),
                     Text(
-                      'Your BMI is too low, you should eat more!',
+                      interpretation,
                       textAlign: TextAlign.center,
                       style: kBodyTextStyle,
                     ),
